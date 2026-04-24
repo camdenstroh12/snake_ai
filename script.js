77aef520a8d157497f062a1feddcf5b802270452
camdenstroh12 <camdenstroh12@gmail.com>
2026-04-24 13:19:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -106,23 +106,44 @@ function update() {
     let target = getClosestFood(snake[0]);
     let path = astar(snake[0], target);
 
+    // move snake
     if (path.length > 1) {
         snake.unshift(path[1]);
     } else {
-        // fallback movement
         let h = snake[0];
         snake.unshift({x:(h.x+1)%gridSize, y:h.y});
     }
 
-    // === FOOD CHECK ===
+    let ateFood = false;
+
+    // check eating
     for (let i = 0; i < foods.length; i++) {
-        if (snake[0].x === foods[i].x && snake[0].y === foods[i].y) {
+        if (
+            snake[0].x === foods[i].x &&
+            snake[0].y === foods[i].y
+        ) {
+            ateFood = true;
+
+            // remove eaten food
             foods.splice(i, 1);
-            foods.push(spawnFood());
-            return; // grow (don't pop)
+
+            break;
         }
     }
 
+    // spawn new food if eaten
+    if (ateFood) {
+        foods.push(spawnFood());
+    } else {
+        // normal movement
+        snake.pop();
+    }
+
+    // 🔥 GUARANTEE 10 foods ALWAYS
+    while (foods.length < 10) {
+        foods.push(spawnFood());
+    }
+}
     // normal move
     snake.pop();
 }
